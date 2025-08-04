2b12e5c63c9162cd57c6e654b08e0c7b5edaab6d
jay candel <133104319+jaylikesbunda@users.noreply.github.com>
2025-08-04 13:41:46 +0800

    Update flasher.js

--- flasher.js
+++ flasher.js
@@ -1324,7 +1324,8 @@ document.addEventListener('DOMContentLoaded', () => {
             "LilyGo-TEmbedC1101.zip": "LilyGo TEmbedC1101",
             "LilyGo-S3TWatch-2020.zip": "LilyGo S3 T-Watch 2020",
             "esp32c5-generic.zip": "Generic ESP32-C5",
-            "esp32c5-generic-v01.zip": "Generic ESP32-C5 (v01)"
+            "esp32c5-generic-v01.zip": "Generic ESP32-C5 (v01)",
+            "TDisplayS3-Touch.zip": "LilyGo TDisplay S3 Touch (ESP32-S3)"
         };
 
         // Mapping from build target (idf_target) to chip name used in 'selectedDevice'
@@ -1366,7 +1367,8 @@ document.addEventListener('DOMContentLoaded', () => {
             "LilyGo-TEmbedC1101.zip": "esp32s3",
             "LilyGo-S3TWatch-2020.zip": "esp32s3",
             "esp32c5-generic.zip": "esp32c5",
-            "esp32c5-generic-v01.zip": "esp32c5"
+            "esp32c5-generic-v01.zip": "esp32c5",
+            "TDisplayS3-Touch.zip": "esp32s3"
         };
 
         // --- Helper function to populate assets into a parent element ---
